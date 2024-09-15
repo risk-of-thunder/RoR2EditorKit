707f91bbb5f40861f24602999793595c6f1fe0e7
TheTimeSweeper <leo.midget@gmail.com>
2024-09-15 12:03:06 -0700

    EntityStateMachine and GenericSkill drawers to clarify which one is selected

---
+++ Editor/RoR2/PropertyDrawers/EntityStateMachineDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace RoR2.Editor.PropertyDrawers
+{
+    [CustomPropertyDrawer(typeof(EntityStateMachine))]
+    public class EntityStateMachineDrawer : NamedObjectReferencePropertyDrawer<EntityStateMachine>
+    {
+        protected override string GetName(EntityStateMachine property)
+        {
+            return property.customName;
+        }
+    }
+}
---
+++ Editor/RoR2/PropertyDrawers/GenericSkillDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace RoR2.Editor.PropertyDrawers
+{
+    [CustomPropertyDrawer(typeof(GenericSkill))]
+    public class GenericSkillDrawer : NamedObjectReferencePropertyDrawer<GenericSkill>
+    {
+        protected override string GetName(GenericSkill property)
+        {
+            return property.skillName;
+        }
+    }
+}
